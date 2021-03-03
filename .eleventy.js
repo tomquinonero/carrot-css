41f4c4801d577969953bcde1e43b618ea8fde9f1
Tom Quinonero <tomquinonero@hey.com>
2021-03-03 03:40:46 +0100

    config eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -4,11 +4,11 @@ const util = require('util')
 const moment = require('moment')
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget('main.css')
+  // eleventyConfig.addWatchTarget('main.css')
 
   // design images
   // eleventyConfig.addPassthroughCopy({ public: 'public' })
-  eleventyConfig.addPassthroughCopy('_dist')
+  // eleventyConfig.addPassthroughCopy('_dist')
 
   // eleventyConfig.addCollection('sortedGallery', function (collectionApi) {
   //   return collectionApi.getFilteredByTag('gallery').sort(function (a, b) {
@@ -16,28 +16,34 @@ module.exports = function (eleventyConfig) {
   //   })
   // })
 
-  // eleventyConfig.addPlugin(pluginSass, {
-  //   // keep only "css/main.scss" in prod
-  //   watch: ['css/**/*.css', 'css/main.scss'],
-  //   // watch: ["css/main.scss"],
-  //   sourcemaps: true,
-  //   outputDir: '_site/css',
+  eleventyConfig.addPlugin(pluginSass, {
+    // keep only "css/main.scss" in prod
+    watch: [
+      'css/main.scss',
+      'css/*.css',
+      'css/*.scss',
+      'css/**/*.css',
+      'css/**/*.scss',
+    ],
+    // watch: ["css/main.scss"],
+    sourcemaps: true,
+    outputDir: 'docs/_site/css/',
+  })
+
+  // eleventyConfig.addFilter('console', function (value) {
+  //   return util.inspect(value)
   // })
 
-  eleventyConfig.addFilter('console', function (value) {
-    return util.inspect(value)
-  })
+  // eleventyConfig.addFilter('getPageBySlug', function (collection, slug) {
+  //   let item = collection.find(function (item) {
+  //     return item.data.page.fileSlug == slug
+  //   })
+  //   return item ? item.data : null
+  // })
 
-  eleventyConfig.addFilter('getPageBySlug', function (collection, slug) {
-    let item = collection.find(function (item) {
-      return item.data.page.fileSlug == slug
-    })
-    return item ? item.data : null
-  })
-
-  eleventyConfig.addFilter('formatDate', function (date) {
-    return moment(date).format('DD.MM.YYYY')
-  })
+  // eleventyConfig.addFilter('formatDate', function (date) {
+  //   return moment(date).format('DD.MM.YYYY')
+  // })
 
   return {
     dir: {
